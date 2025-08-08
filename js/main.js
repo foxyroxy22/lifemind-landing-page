036c587c8ca69ebf00b9b329d6a85e6cfe5350b7
foxyroxy22 <dkwlsdl9@gmail.com>
2025-08-08 15:27:24 +0900

    aos 추가, 헤더 배경 블러, gnb a 눌러서 해당 영역으로 스크롤 되게

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
   // aos 연결
   AOS.init();
+
+  $('.gnb a').click(function(e) {
+    e.preventDefault(); // 기본 앵커 동작 방지
+    var target = $(this.hash); // 클릭한 링크의 href 값 가져오기
+    
+    if (target.length) {
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 800); // 800ms 동안 부드럽게 이동
+    }
+});
+
+$(window).scroll(function() {
+    var scrollPos = $(document).scrollTop();
+    
+    $('.gnb a').each(function() {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        
+        if (refElement.position().top <= scrollPos + 100 && 
+            refElement.position().top + refElement.height() > scrollPos + 100) {
+            $('.gnb a').removeClass("active");
+            currLink.addClass("active");
+        }
+    });
+});
 });
